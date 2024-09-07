c8c9443b78c322d27053d7b948e473a633e1d44c
Martin Colbourne <martinlambc@gmail.com>
2024-09-07 23:39:46 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import requests
+import polars as pl
+import matplotlib.pyplot as plt
+
+st.title("Chess.com Comparator")
+
+st.text_input("Enter your Chess.com username", key="username")
+
+username = st.session_state.username
+
+user = requests.get(f'https://api.chess.com/pub/player/{username}/stats').json()
