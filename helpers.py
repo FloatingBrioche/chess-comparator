cba4941de0d115bbafea6220c825f28f4e5f0623
Martin Colbourne <martinlambc@gmail.com>
2024-09-08 11:15:03 +0100

    add get stats helper function

---
+++ helpers.py
@@ -0,0 +1,21 @@
+import requests
+import polars
+
+
+def get_stats(username: str) -> dict | None:
+    '''
+    '''
+    headers = {'user-agent': 'chess-comparator'}
+    
+    url = f'https://api.chess.com/pub/player/{username}/stats'
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def dict_to_df():
+    pass
