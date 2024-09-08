173173600e6f62e11a1de459fbb19fb3d9866e92
Martin Colbourne <martinlambc@gmail.com>
2024-09-08 11:42:17 +0100

    add get_profile tests

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,17 +1,30 @@
 import pytest
 from helpers import get_profile as gp
+from helpers import get_stats as gs
 
 
-@pytest.mark.it("Returns dict for valid username")
-def test_returns_dict():
-    result = gp("Aporian")
-    assert isinstance(result, dict)
+class TestGetStats:
+    @pytest.mark.it("Returns dict for valid username")
+    def test_returns_dict(self):
+        result = gs("Aporian")
+        assert isinstance(result, dict)
 
+    @pytest.mark.it("Returns None for invalid username")
+    def test_returns_None(self):
+        result = gs("Aporztian")
+        assert result is None
+        
 
-@pytest.mark.it("Returns None for invalid username")
-def test_returns_None():
-    result = gp("Aporztian")
-    assert result is None
+class TestGetProfile:
+    @pytest.mark.it("Returns dict for valid username")
+    def test_returns_dict(self):
+        result = gp("Aporian")
+        assert isinstance(result, dict)
+
+    @pytest.mark.it("Returns None for invalid username")
+    def test_returns_None(self):
+        result = gp("Aporztian")
+        assert result is None
 
 
 
