8652b392c65fb1d25e509b5404033e597b3f1e7d
Martin Colbourne <martinlambc@gmail.com>
2024-09-08 11:42:10 +0100

    add get_profile function

--- helpers.py
+++ helpers.py
@@ -15,6 +15,21 @@ def get_stats(username: str) -> dict | None:
         return response.json()
     else:
         return None
+    
+
+def get_profile(username: str) -> dict | None:
+    '''
+    '''
+    headers = {'user-agent': 'chess-comparator'}
+    
+    url = f'https://api.chess.com/pub/player/{username}'
+
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 
 def dict_to_df():
