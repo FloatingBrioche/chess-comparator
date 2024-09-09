0a9852cb9fa1ff107d89dad806ab99822ebdde97
Martin Colbourne <martinlambc@gmail.com>
2024-09-09 11:59:27 +0100

    add logging tests

--- test/test_request_helpers.py
+++ test/test_request_helpers.py
@@ -1,5 +1,7 @@
 import pytest
 from request_helpers import get_profile as gp, get_stats as gs
+from unittest.mock import patch
+from requests.exceptions import RequestException
 
 
 class TestGetProfile:
@@ -24,3 +26,15 @@ class TestGetStats:
     def test_returns_None(self):
         result = gs("Aporztian")
         assert result is None
+
+
+class TestLogging:
+    @patch("request_helpers.api_get", side_effect=RequestException("Test exception"))
+    def test_get_profile_logging(self, mock_api_get, caplog):
+        gp("Aporian")
+        assert "Request error" in caplog.text
+
+    @patch("request_helpers.api_get", side_effect=RequestException("Test exception"))
+    def test_get_stats_logging(self, mock_api_get, caplog):
+        gs("Aporian")
+        assert "Request error" in caplog.text
