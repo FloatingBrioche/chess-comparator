f862b2840db640bb0c0f0227a896fa16b1f790fa
Martin Colbourne <martinlambc@gmail.com>
2024-09-09 20:12:31 +0100

    refactor to strip 'chess' prefix and change df orientation

--- data_helpers.py
+++ data_helpers.py
@@ -6,9 +6,9 @@ def get_current_v_best(user_stats):
     for k, v in user_stats.items():
         if "last" in v:
             if "rating" in v["last"]:
-                stats["current"][k] = v["last"]["rating"]
+                stats["current"][k.removeprefix('chess_')] = v["last"]["rating"]
         if "best" in v:
             if "rating" in v["best"]:
-                stats["best"][k] = v["best"]["rating"]
-    return pd.DataFrame.from_dict(stats, orient="index")
+                stats["best"][k.removeprefix('chess_')] = v["best"]["rating"]
+    return pd.DataFrame.from_dict(stats, orient="columns")
 
