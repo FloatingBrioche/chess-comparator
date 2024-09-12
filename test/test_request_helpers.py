a898ff5a097d01ce2faf09ceb8d1d903b0528c71
Martin Colbourne <martinlambc@gmail.com>
2024-09-12 17:34:53 +0100

    add tests for get request args

--- test/test_request_helpers.py
+++ test/test_request_helpers.py
@@ -1,10 +1,27 @@
 import pytest
 from request_helpers import get_profile as gp, get_stats as gs
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from requests.exceptions import RequestException
 
 
+@pytest.fixture
+def mock_response():
+    mock_response = Mock()
+    mock_response.status_code = 500
+    return mock_response
+
+
 class TestGetProfile:
+    @pytest.mark.it("Uses username parameter in get request")
+    @patch("request_helpers.api_get")
+    def test_get_request(self, mock_api_get, mock_response):
+        mock_api_get.return_value = mock_response
+        gp("Aporztian")
+        mock_api_get.assert_called_once_with(
+            "https://api.chess.com/pub/player/Aporztian",
+            headers={"user-agent": "chess-comparator"},
+        )
+
     @pytest.mark.it("Returns dict for valid username")
     def test_returns_dict(self):
         result = gp("Aporian")
@@ -17,6 +34,16 @@ class TestGetProfile:
 
 
 class TestGetStats:
+    @pytest.mark.it("Uses username parameter in get request")
+    @patch("request_helpers.api_get")
+    def test_get_request(self, mock_api_get, mock_response):
+        mock_api_get.return_value = mock_response
+        gs("Aporztian")
+        mock_api_get.assert_called_once_with(
+            "https://api.chess.com/pub/player/Aporztian/stats",
+            headers={"user-agent": "chess-comparator"},
+        )
+
     @pytest.mark.it("Returns dict for valid username")
     def test_returns_dict(self):
         result = gs("Aporian")
