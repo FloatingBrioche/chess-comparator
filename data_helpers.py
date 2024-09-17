a61ef52a0f94545f48f2649aeca91246af020786
Martin Colbourne <martinlambc@gmail.com>
2024-09-17 17:13:39 +0100

    add get_user_v_other function and test suite

--- data_helpers.py
+++ data_helpers.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-def get_current_v_best(user_stats):
+def get_current_v_best(u_stats) -> pd.DataFrame:
     stats = {"current": {}, "best": {}}
-    for k, v in user_stats.items():
+    for k, v in u_stats.items():
         if "last" in v:
             if "rating" in v["last"]:
                 stats["current"][k.removeprefix('chess_')] = v["last"]["rating"]
@@ -12,3 +12,41 @@ def get_current_v_best(user_stats):
                 stats["best"][k.removeprefix('chess_')] = v["best"]["rating"]
     return pd.DataFrame.from_dict(stats, orient="columns")
 
+
+def get_user_v_other(user: dict, other: dict) -> pd.DataFrame:
+    user_name, other_name = list(user.keys())[0], list(other.keys())[0]
+    u_stats, oth_stats = user[user_name], other[other_name]
+    stats = {user_name: {}, other_name: {}}
+    u, oth = stats[user_name], stats[other_name]
+    available_user_metrics = set(u_stats.keys())
+    available_other_metrics = set(oth_stats.keys())
+    comparable_metrics = available_user_metrics.intersection(available_other_metrics)
+    
+    for metric in comparable_metrics:
+        metric_name = metric.removeprefix('chess_')
+        if "record" in u_stats[metric]:
+            u[metric_name + "_best"] = u_stats[metric]["best"]["rating"]
+            u[metric_name + "_current"] = u_stats[metric]["last"]["rating"]
+            u[metric_name + "_wins"] = u_stats[metric]["record"]["win"]
+            u[metric_name + "_draws"] = u_stats[metric]["record"]["draw"]            
+            u[metric_name + "_losses"] = u_stats[metric]["record"]["loss"]
+            
+            oth[metric_name + "_best"] = oth_stats[metric]["best"]["rating"]
+            oth[metric_name + "_current"] = oth_stats[metric]["last"]["rating"] 
+            oth[metric_name + "_wins"] = oth_stats[metric]["record"]["win"]   
+            oth[metric_name + "_draws"] = oth_stats[metric]["record"]["draw"]              
+            oth[metric_name + "_losses"] = oth_stats[metric]["record"]["loss"]
+
+        if metric == "fide":
+            u["FIDE"] = u_stats["fide"]
+            oth["FIDE"] = oth_stats["fide"]
+        
+        if metric == "puzzle_rush":
+            u[metric] = u_stats[metric]["best"]["score"]
+            oth[metric] = oth_stats[metric]["best"]["score"]
+
+        if metric == "tactics":
+            u["puzzles_best_rating"] = u_stats[metric]["highest"]["rating"]
+            oth["puzzles_best_rating"] = oth_stats[metric]["highest"]["rating"]
+
+    return pd.DataFrame.from_dict(stats, orient="columns")
--- test/test_data_helpers.py
+++ test/test_data_helpers.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 from json import load
-from data_helpers import get_current_v_best
+from data_helpers import get_current_v_best, get_user_v_other
 
 
 @pytest.fixture()
@@ -12,36 +12,82 @@ def profile():
 
 
 @pytest.fixture()
-def stats():
+def user_stats():
     with open("test/test_data/test_stats.json", "r") as file:
-        stats = load(file)
-    return stats
+        user_stats = load(file)
+    return user_stats
+
+
+@pytest.fixture()
+def other_stats():
+    with open("test/test_data/test_other_stats.json", "r") as file:
+        other_stats = load(file)
+    return other_stats
 
 
 class TestCurrentVBest:
-    def test_returns_data_frame(self, stats):
-        result = get_current_v_best(stats)
+    def test_returns_data_frame(self, user_stats):
+        result = get_current_v_best(user_stats)
         assert isinstance(result, pd.DataFrame)
 
     @pytest.mark.it("Df has 'current' and 'best' as column names")
-    def test_data_frame_has_expected_col_names(self, stats):
-        df = get_current_v_best(stats)
-        expected_cols = [
-            "current",
-            "best"   
-        ]
+    def test_data_frame_has_expected_col_names(self, user_stats):
+        df = get_current_v_best(user_stats)
+        expected_cols = ["current", "best"]
         output_cols = df.columns.to_list()
         assert expected_cols == output_cols
-    
+
     @pytest.mark.it("Df has game types as indices")
-    def test_data_frame_has_expected_row_names(self, stats):
-        df = get_current_v_best(stats)
-        expected_rows = [
-            "daily",
-            "chess960_daily",
-            "rapid",
-            "bullet",
-            "blitz"
-        ]
+    def test_data_frame_has_expected_row_names(self, user_stats):
+        df = get_current_v_best(user_stats)
+        expected_rows = ["daily", "chess960_daily", "rapid", "bullet", "blitz"]
         output_rows = df.index.to_list()
         assert expected_rows == output_rows
+
+
+class TestUserVOther:
+    def test_returns_data_frame(self, user_stats, other_stats):
+        result = get_user_v_other({"Mazza": user_stats}, {"Cazza": other_stats})
+        assert isinstance(result, pd.DataFrame)
+
+    @pytest.mark.it("Df has passed user and other names as column names")
+    def test_data_frame_has_expected_col_names(self, user_stats, other_stats):
+        df = get_user_v_other({"Mazza": user_stats}, {"Cazza": other_stats})
+        expected_cols = ["Mazza", "Cazza"]
+        assert expected_cols == df.columns.to_list()
+
+    @pytest.mark.it("Df has metrics available for both users (only) as indices")
+    def test_data_frame_has_expected_row_names(self, user_stats, other_stats):
+        df = get_user_v_other({"Mazza": user_stats}, {"Cazza": other_stats})
+        expected_rows = [
+            "blitz_best",
+            "blitz_current",
+            "blitz_wins",
+            "blitz_draws",
+            "blitz_losses",
+            "daily_best",
+            "daily_current",
+            "daily_wins",
+            "daily_draws",
+            "daily_losses",
+            "rapid_best",
+            "rapid_current",
+            "rapid_wins",
+            "rapid_draws",
+            "rapid_losses",
+            "puzzle_rush",
+            "puzzles_best_rating",
+        ]
+        output_rows = df.index.to_list()
+        assert set(expected_rows) == set(output_rows)
+
+    @pytest.mark.it("Df populates rows with expected data from passed dictionaries")
+    def test_row_data(self, user_stats, other_stats):
+        df = get_user_v_other({"Mazza": user_stats}, {"Cazza": other_stats})
+        expected_values = {
+            "puzzles_best_rating": [2593, 1808],
+            "daily_best": [1584, 1278],
+            "rapid_wins": [72, 2111],
+        }
+        for k, v in expected_values.items():
+            assert df.loc[k].tolist() == v
