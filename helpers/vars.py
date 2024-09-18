be7d3719635343c4bf859a0fed78785031c872e9
Martin Colbourne <martinlambc@gmail.com>
2024-09-18 21:46:15 +0100

    update vars file

--- helpers/vars.py
+++ helpers/vars.py
@@ -1,9 +1,25 @@
 vars = {
     "select_options": [
-        "My best self",
+        "Myself",
         "Another Chess.com user",
         "A random grandmaster",
         "A random person from my country",
     ],
-    "totals": ["total_wins", "total_draws", "total_losses"],
+    "total": ["total_wins", "total_draws", "total_losses"],
+    "daily": ["daily_wins", "daily_draws", "daily_losses"],
+    "blitz": ["blitz_wins", "blitz_draws", "blitz_losses"],
+    "rapid": ["rapid_wins", "rapid_draws", "rapid_losses"],
+    "bullet": ["bullet_wins", "bullet_draws", "bullet_losses"],
+    "ratings": [
+        "daily_best",
+        "daily_current",
+        "rapid_best",
+        "rapid_current",
+        "blitz_best",
+        "blitz_current",
+        "bullet_best",
+        "bullet_current",
+        "FIDE",
+        "puzzles_best_rating"
+    ]
 }
