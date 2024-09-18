e85468415b80178ca2050ac5b3fbab8e98401ade
Martin Colbourne <martinlambc@gmail.com>
2024-09-18 13:09:40 +0100

    add vars helper

---
+++ helpers/vars.py
@@ -0,0 +1,9 @@
+vars = {
+    "select_options": [
+        "My best self",
+        "Another Chess.com user",
+        "A random grandmaster",
+        "A random person from my country",
+    ],
+    "totals": ["total_wins", "total_draws", "total_losses"],
+}
