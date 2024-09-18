71c3b9a2fd7037a74cece60184bc90cda1c81bde
Martin Colbourne <martinlambc@gmail.com>
2024-09-18 21:46:48 +0100

    minor update

--- test/test_plot_helpers.py
+++ test/test_plot_helpers.py
@@ -14,9 +14,9 @@ def expanded_df():
 
 class TestPlotPie:
     def test_returns_plottable_object(self, expanded_df):
-        output = plot_pie(expanded_df, "Mazza", vars["totals"])
-        assert isinstance(output.figure, Figure)
+        output = plot_pie(expanded_df, "Mazza", vars["total"])
+        assert isinstance(output, Figure)
 
     def test_takes_passed_name_as_y_label(self, expanded_df):
-        output = plot_pie(expanded_df, "Cazza", vars["totals"])
+        output = plot_pie(expanded_df, "Cazza", vars["total"])
         assert output.get_axes()[0].get_ylabel() == "Cazza"
