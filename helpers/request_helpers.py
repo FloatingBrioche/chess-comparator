f7285b191df823b38d02292316a3231fe9ab5e1d
Martin Colbourne <martinlambc@gmail.com>
2024-09-18 21:45:42 +0100

    add get_compatriots and get_gms functions

--- helpers/request_helpers.py
+++ helpers/request_helpers.py
@@ -1,5 +1,8 @@
 from requests import get as get_request
 from requests.exceptions import RequestException
+from os.path import isfile
+from json import load, dump
+from random import choice
 import logging
 
 
@@ -102,3 +105,54 @@ def get_gms() -> list:
 
     except RequestException as e:
         logger.error(f"Request error: {e}")
+
+
+def get_compatriots(iso: str):
+    """
+    Uses the requests library to call the Chess.com API
+    and retrieve JSON data of the users from a particular country.
+
+    If the request receives a 200 response,
+    the JSON is converted to a list and returned. If not, returns None.
+
+    Parameters:
+    - iso: str
+
+    Returns either:
+    - A list of chess.com users (for a 200 response)
+    - None (for any other response)
+    """
+    try:
+        url = f"https://api.chess.com/pub/country/{iso}/players"
+
+        response = get_request(url, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()["players"]
+        else:
+            return None
+
+    except RequestException as e:
+        logger.error(f"Request error: {e}")    
+
+
+def get_random_gm():
+    if isfile("./storage/saved_gms.json"):
+        with open("./storage/saved_gms.json", "r") as jsizzle:
+            gms = load(jsizzle)
+    else:
+        gms = get_gms()
+        with open("./storage/saved_gms.json", "w") as jsizzle:
+            dump(gms, jsizzle)
+    return choice(gms)
+
+
+def get_random_compatriot(iso):
+    if isfile(f'./storage/{iso}.json'):
+        with open(f"./storage/{iso}.json", "r") as jsizzle:
+            compatriots = load(jsizzle)
+    else:
+        compatriots = get_compatriots(iso)
+        with open(f"./storage/{iso}.json", "w") as jsizzle:
+            dump(compatriots, jsizzle)
+    return choice(compatriots)
