751a2330b1322f262f1d6ec3f0bded2f07352d87
Martin Colbourne <martinlambc@gmail.com>
2024-09-22 22:00:59 +0100

    refactor vars

--- helpers/vars.py
+++ helpers/vars.py
@@ -1,16 +1,10 @@
-vars = {
-    "select_options": [
-        "Myself",
-        "Another Chess.com user",
-        "A random grandmaster",
-        "A random person from my country",
-    ],
-    "total": ["total_wins", "total_draws", "total_losses"],
-    "daily": ["daily_wins", "daily_draws", "daily_losses"],
-    "blitz": ["blitz_wins", "blitz_draws", "blitz_losses"],
-    "rapid": ["rapid_wins", "rapid_draws", "rapid_losses"],
-    "bullet": ["bullet_wins", "bullet_draws", "bullet_losses"],
-    "ratings": [
+blitz_indices = ["blitz_wins", "blitz_draws", "blitz_losses"]
+bullet_indices = ["bullet_wins", "bullet_draws", "bullet_losses"]
+daily_indices = ["daily_wins", "daily_draws", "daily_losses"]
+rapid_indices = ["rapid_wins", "rapid_draws", "rapid_losses"]
+totals_indices = ["total_wins", "total_draws", "total_losses"]
+
+ratings_indices = [
         "daily_best",
         "daily_current",
         "rapid_best",
@@ -22,4 +16,10 @@ vars = {
         "FIDE",
         "puzzles_best_rating"
     ]
-}
+
+select_options = [
+        "Myself",
+        "Another Chess.com user",
+        "A random grandmaster",
+        "A random person from my country"
+    ]
