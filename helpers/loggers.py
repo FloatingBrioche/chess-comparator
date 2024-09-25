7ec321489b98819269ccbee8f7b699cd703d60d1
Martin Colbourne <martinlambc@gmail.com>
2024-09-25 17:45:43 +0100

    add loggers.py

---
+++ helpers/loggers.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(
+    filename="errors.log",
+    filemode="a",
+    level=logging.ERROR,
+    format="%(asctime)s – %(name)s. %(message)s",
+    datefmt="%d-%b-%Y %H:%M:%S"
+)
+
+data_logger = logging.getLogger("Data logger")
+
+request_logger = logging.getLogger("Request logger")
