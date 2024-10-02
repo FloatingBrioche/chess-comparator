f433acf7335a116d92d4a63c816aa75733c87a62
Martin Colbourne <martinlambc@gmail.com>
2024-10-02 19:41:36 +0200

    refactor imports

--- test/test_plot_helpers.py
+++ test/test_plot_helpers.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from helpers.plot_helpers import plot_pie
-from helpers.vars import vars
+from helpers.vars import totals_indices
 
 
 @pytest.fixture()
@@ -14,9 +14,9 @@ def expanded_df():
 
 class TestPlotPie:
     def test_returns_plottable_object(self, expanded_df):
-        output = plot_pie(expanded_df, "Mazza", vars["total"])
+        output = plot_pie(expanded_df, "Mazza", totals_indices)
         assert isinstance(output, Figure)
 
     def test_takes_passed_name_as_y_label(self, expanded_df):
-        output = plot_pie(expanded_df, "Cazza", vars["total"])
+        output = plot_pie(expanded_df, "Cazza", totals_indices)
         assert output.get_axes()[0].get_ylabel() == "Cazza"
--- test/test_request_helpers.py
+++ test/test_request_helpers.py
@@ -13,7 +13,7 @@ def mock_response():
 
 class TestGetProfile:
     @pytest.mark.it("Uses username parameter in get request")
-    @patch("request_helpers.get_request")
+    @patch("helpers.request_helpers.get_request")
     def test_get_request(self, mock_api_get, mock_response):
         mock_api_get.return_value = mock_response
         get_profile("Aporztian")
@@ -35,7 +35,7 @@ class TestGetProfile:
 
 class TestGetStats:
     @pytest.mark.it("Uses username parameter in get request")
-    @patch("request_helpers.get_request")
+    @patch("helpers.request_helpers.get_request")
     def test_get_request(self, mock_api_get, mock_response):
         mock_api_get.return_value = mock_response
         get_stats("Aporztian")
@@ -66,17 +66,17 @@ class TestGetGMs:
 
 
 class TestLogging:
-    @patch("request_helpers.get_request", side_effect=RequestException("Test exception"))
+    @patch("helpers.request_helpers.get_request", side_effect=RequestException("Test exception"))
     def test_logs_get_profile_request_exceptions(self, mock_api_get, caplog):
         get_profile("Aporian")
         assert "Request error" in caplog.text
 
-    @patch("request_helpers.get_request", side_effect=RequestException("Test exception"))
+    @patch("helpers.request_helpers.get_request", side_effect=RequestException("Test exception"))
     def test_logs_get_stats_request_exceptions(self, mock_api_get, caplog):
         get_stats("Aporian")
         assert "Request error" in caplog.text
 
-    @patch("request_helpers.get_request", side_effect=RequestException("Test exception"))
+    @patch("helpers.request_helpers.get_request", side_effect=RequestException("Test exception"))
     def test_logs_get_gms_request_exceptions(self, mock_api_get, caplog):
         get_gms()
         assert "Request error" in caplog.text
