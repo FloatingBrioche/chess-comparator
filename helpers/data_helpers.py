0318189d0b2e942e7c714a49d5f0752dc23a5f0c
Martin Colbourne <martinlambc@gmail.com>
2024-10-02 19:36:54 +0200

    refactor to expand error logging and avoid key errors

--- helpers/data_helpers.py
+++ helpers/data_helpers.py
@@ -6,15 +6,19 @@ def get_current_v_best(u_stats) -> pd.DataFrame:
     try:
         stats = {"current": {}, "best": {}}
         for k, v in u_stats.items():
-            if "last" in v:
-                if "rating" in v["last"]:
-                    stats["current"][k.removeprefix("chess_")] = v["last"]["rating"]
-            if "best" in v:
-                if "rating" in v["best"]:
-                    stats["best"][k.removeprefix("chess_")] = v["best"]["rating"]
+            if isinstance(v, dict):
+                if "last" in v:
+                    if "rating" in v["last"]:
+                        stats["current"][k.removeprefix("chess_")] = v["last"]["rating"]
+                if "best" in v:
+                    if "rating" in v["best"]:
+                        stats["best"][k.removeprefix("chess_")] = v["best"]["rating"]
         return pd.DataFrame.from_dict(stats, orient="columns")
-    except (KeyError, TypeError) as e:
-        data_logger.error(f"Error in {get_current_v_best.__name__}: {str(e)}")
+    except KeyError as e:
+        data_logger.error(f"Key error in {get_current_v_best.__name__}: {str(e)}, key = {k}, value = {v}")
+        raise e
+    except TypeError as e:
+        data_logger.error(f"Type error in {get_current_v_best.__name__}: {str(e)}, key = {k}, value = {v}")
         raise e
 
 def get_user_v_other(user: dict, other: dict) -> pd.DataFrame:
@@ -34,36 +38,46 @@ def get_user_v_other(user: dict, other: dict) -> pd.DataFrame:
 
         # Iterates over common metrics, looks up values and adds k-vs to stats dict
         for metric in comparable_metrics:
+            
             metric_name = metric.removeprefix("chess_")
-            if "record" in u_stats[metric]:
-                u[metric_name + "_best"] = u_stats[metric]["best"]["rating"]
+            
+            if metric == "fide":
+                u["FIDE"] = u_stats["fide"]
+                oth["FIDE"] = oth_stats["fide"]
+
+            elif metric == "puzzle_rush":
+                if "best" in u_stats[metric] and "best" in oth_stats[metric]:
+                    u[metric] = u_stats[metric]["best"]["score"]
+                    oth[metric] = oth_stats[metric]["best"]["score"]
+
+            elif metric == "tactics":
+                if "highest" in u_stats[metric] and "highest" in oth_stats[metric]:
+                    u["puzzles"] = u_stats[metric]["highest"]["rating"]
+                    oth["puzzles"] = oth_stats[metric]["highest"]["rating"]
+            
+            elif "record" in u_stats[metric]:                
                 u[metric_name + "_current"] = u_stats[metric]["last"]["rating"]
                 u[metric_name + "_wins"] = u_stats[metric]["record"]["win"]
                 u[metric_name + "_draws"] = u_stats[metric]["record"]["draw"]
                 u[metric_name + "_losses"] = u_stats[metric]["record"]["loss"]
-
-                oth[metric_name + "_best"] = oth_stats[metric]["best"]["rating"]
+                
                 oth[metric_name + "_current"] = oth_stats[metric]["last"]["rating"]
                 oth[metric_name + "_wins"] = oth_stats[metric]["record"]["win"]
                 oth[metric_name + "_draws"] = oth_stats[metric]["record"]["draw"]
                 oth[metric_name + "_losses"] = oth_stats[metric]["record"]["loss"]
 
-            if metric == "fide":
-                u["FIDE"] = u_stats["fide"]
-                oth["FIDE"] = oth_stats["fide"]
+                if "best" in u_stats[metric] and "best" in oth_stats[metric]:
+                    u[metric_name + "_best"] = u_stats[metric]["best"]["rating"]
 
-            if metric == "puzzle_rush":
-                u[metric] = u_stats[metric]["best"]["score"]
-                oth[metric] = oth_stats[metric]["best"]["score"]
-
-            if metric == "tactics":
-                u["puzzles"] = u_stats[metric]["highest"]["rating"]
-                oth["puzzles"] = oth_stats[metric]["highest"]["rating"]
+                    oth[metric_name + "_best"] = oth_stats[metric]["best"]["rating"]
 
         # Converts stats dict to dataframe with usernames as  columns
         return pd.DataFrame.from_dict(stats, orient="columns")
-    except (KeyError, TypeError) as e:
-        data_logger.error(f"Error in {get_user_v_other.__name__}: {str(e)}")
+    except KeyError as e:
+        data_logger.error(f"Key error in {get_user_v_other.__name__}: {str(e)}, metric = {metric}, username = {user_name}, other_username = {other_name}")
+        raise e
+    except TypeError as e:
+        data_logger.error(f"Type error in {get_user_v_other.__name__}: {str(e)}, metric = {metric}, username = {user_name}, other_username = {other_name}")
         raise e
 
 
